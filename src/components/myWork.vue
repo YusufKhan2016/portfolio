527ed3e349680631c3ca82007260cf5f3e0ac22c
Hasan Yusuf Khan <116283180+YusufKhan2016@users.noreply.github.com>
2025-09-27 23:09:28 +0600

    new updates aded

--- src/components/myWork.vue
+++ src/components/myWork.vue
@@ -12,8 +12,23 @@
         </div>
       </div>
     </div>
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in ex quam deleniti adipisci nihil error architecto porro labore cumque assumenda eum illum dolorum, perferendis eos vero beatae! Sapiente ullam, illum, quam a dicta laborum quaerat eligendi quis nam magni vitae odio enim reiciendis, fugiat expedita quidem? Accusamus pariatur voluptate nostrum aut error itaque deleniti laboriosam. Cupiditate nobis error tenetur architecto. Incidunt tempore blanditiis, fugit facere soluta ipsam veritatis illum maxime alias optio sit tempora praesentium, eius dolorum, dolores necessitatibus sequi exercitationem animi vero expedita aspernatur. Repellendus autem, numquam soluta aut similique exercitationem dolor earum placeat adipisci expedita ex laudantium perspiciatis? Consequuntur hic laudantium nisi saepe dignissimos, exercitationem ipsa aspernatur molestiae corporis non recusandae soluta totam dolorem nesciunt id blanditiis voluptatem ipsum vel velit enim mollitia adipisci vero odio. Veniam, maiores voluptate. Atque corporis veritatis, omnis facilis neque quia dicta consectetur voluptatum culpa qui itaque quod exercitationem id officia quis officiis distinctio suscipit tempora at excepturi error rem, corrupti debitis. Sapiente laborum maxime reiciendis, sint modi ullam? Incidunt velit odit repellat pariatur, sed fugit architecto aspernatur quo veniam quaerat. Sit dolore, facere officiis assumenda at, reprehenderit similique commodi perferendis reiciendis nisi soluta, nihil architecto minus ipsum id omnis qui quos ex nostrum consectetur saepe dignissimos! Natus accusamus obcaecati illo animi voluptatibus aliquid labore, optio incidunt sed vero ad rerum temporibus iusto quod similique asperiores eum, odio qui adipisci officia sapiente ratione! Pariatur voluptatibus culpa suscipit, quam repellat nisi aliquam atque non asperiores commodi, sint quidem voluptatem laborum animi autem eveniet reiciendis at debitis nulla necessitatibus. Iure dolores ducimus, totam architecto ab saepe accusantium corporis error quibusdam accusamus, distinctio, odit consectetur mollitia velit facere tempore nostrum in consequatur qui assumenda nobis eum voluptatem! Eum laboriosam quod repellat, odit ad tempora, delectus architecto minus voluptate esse repudiandae? Temporibus numquam quia et nisi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi earum hic quas, beatae facere dolores, consequatur nesciunt reprehenderit inventore culpa perferendis explicabo cumque voluptatibus quos dolor. Sit dicta dolorum sed minus rerum. Fugit sunt expedita qui ratione, ex quod, modi vel unde quis deserunt beatae quae. Repudiandae eligendi eveniet doloribus nam dicta sequi earum, nostrum sint ut impedit laudantium hic at molestias officia consectetur labore quibusdam autem totam aliquam. Necessitatibus beatae dolor provident, consequatur molestiae, quam voluptatibus eveniet optio quos incidunt explicabo delectus! Voluptate neque accusamus voluptatibus cum sit placeat inventore eum, sed ex sequi incidunt, et tempora explicabo doloremque nobis exercitationem obcaecati molestias quam illum qui sapiente modi voluptas! Nobis qui vitae eos, excepturi exercitationem iste non dolorum esse totam ullam. Provident est neque eveniet magnam quisquam ab voluptatibus tempora illum voluptatem doloremque, laborum fugit? Accusamus fugit, assumenda quo ducimus minus enim libero dolore sit quas. Dolor cupiditate non praesentium repudiandae itaque eum ratione tempora illum necessitatibus quidem fuga est ullam sapiente recusandae expedita id ipsam ab nostrum omnis nihil, sint quod? Temporibus, et? Facilis id, minus qui quibusdam laborum eligendi blanditiis repellat totam est quos ad exercitationem sapiente. Ipsam aliquam facere cumque placeat praesentium itaque ex dignissimos repellendus quasi, voluptas soluta, inventore amet sequi. Rem vitae debitis, placeat consequuntur blanditiis quas accusantium similique doloribus dicta iure sit rerum fugit deleniti tempora incidunt, nulla, nostrum eius aspernatur voluptatum obcaecati corrupti laudantium illum? Rem nobis minus asperiores ullam! Accusamus sed doloremque et ullam necessitatibus? Nulla eaque facilis adipisci perspiciatis quam nam, minima repudiandae provident dolor consectetur quis ducimus sequi iste deserunt tempora illum expedita illo fuga delectus, ea corporis optio quidem? Ipsa, deserunt nisi? Ex facere reprehenderit tempora accusantium ab saepe illum eligendi unde corrupti eveniet velit reiciendis aut aperiam possimus dolor, consequatur commodi porro cupiditate similique doloribus officia. Magni.
+
+    <div class="overflow-hidden">
+      <div class="h-[150vh] scale-[1.2]"
+      ref="work">
+        <div
+        v-for="(imageidx, idx) in flexImages"
+        :key="idx"
+        class="flex space-x-4 space-y-4">
+          <div
+          v-for="(img, childIdx) in imageidx"
+          :key="childIdx"
+          class="flex flex-col rounded-lg overflow-hidden"
+          :class="childIdx===1 ? 'translate-y-15' : 'translate-y-0'">
+            <img :src="img" alt="">
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -22,9 +37,30 @@
 import gsap, { ScrollTrigger } from 'gsap/all';
 
 import { ArrowRight } from 'lucide-vue-next';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
+
+import work1 from "@/assets/images/myWork/work1.webp";
+import work2 from "@/assets/images/myWork/work2.webp";
+import work3 from "@/assets/images/myWork/work3.webp";
+import work4 from "@/assets/images/myWork/work4.webp";
+import work5 from "@/assets/images/myWork/work5.webp";
+import work6 from "@/assets/images/myWork/work6.webp";
+import work7 from "@/assets/images/myWork/work7.webp";
+import work8 from "@/assets/images/myWork/work8.webp";
 
 const sectionBorder = ref<HTMLElement | null>(null);
+const work = ref<HTMLElement | null>(null);
+const workImages = ref<string[]>([ work1, work2, work3, work4, work5, work6, work7, work8, work1 ])
+
+const flexImages = computed(() => {
+  const result: string[][] = [];
+
+  for(let i =0;i<workImages.value.length; i+=3) {
+    result.push(workImages.value.slice(i, i+3));
+  }
+
+  return result;
+});
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -34,6 +70,11 @@ onMounted(() => {
     transformOrigin: "center center",
     duration: 2,
   })
+  const workAnimation = gsap.fromTo(
+    work.value,
+    { scale:2 },
+    { scale: 1.1 }
+  )
 
   ScrollTrigger.create({
     trigger: sectionBorder.value,
@@ -41,6 +82,14 @@ onMounted(() => {
     animation: sectionBorderAnimation,
     start: "top 90%",
     end: "top 30%",
+  })
+  ScrollTrigger.create({
+    trigger: work.value,
+    scrub:2,
+    animation: workAnimation,
+    start: "top 80%",
+    end: "bottom 20%",
+    // markers:true,
   })
 
 })
