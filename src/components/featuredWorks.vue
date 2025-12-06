1f7efc6be514a3b57561dc61f61243c76fa6fd4c
Hasan Yusuf Khan <hasanrafsun5@gmail.com>
2025-12-06 23:51:29 +0600

    featured works updated && git push

--- src/components/featuredWorks.vue
+++ src/components/featuredWorks.vue
@@ -1,26 +1,52 @@
 <template>
-  <section ref="sectionRef">
+  <section>
     <div class="container mx-auto px-27 py-40">
-      <div class="flex flex-col">
-        <div class="flex">
+      <hr
+      class="border-1 border-solid border-amber-50"
+      ref="sectionBorder">
 
-          <div class="flex flex-col items-center">
-            <MainHeadingText
-            title="Featured"
-            title2="Works"
-            subtitle="Portfolio"
-            align="center"
-            />
-          </div>
+      <div class="flex flex-col items-center">
+        <div class="flex my-24">
+
+          <MainHeadingText
+          title="Featured"
+          title2="Works"
+          subtitle="Portfolio"
+          align="center"
+          />
+
         </div>
 
         <div class="mx-auto">
           <div
-          v-for="(data, idx) in experiencesData"
+          v-for="(data, idx) in featuredWorksData"
           :key="idx"
-          :ref="el => experiences[idx] = el as HTMLElement"
-          class="sticky top-10 bg-amber-800 w-[800px] h-[550px] rounded-2xl"
+          :ref="el => featuredWork[idx] = el as HTMLElement"
+          class="sticky top-10 bg-amber-50 w-[900px] h-[550px]  rounded-2xl my-4 overflow-hidden"
           >
+          <div class="relative w-full h-full group">
+              <div class="absolute z-30 w-full flex justify-between mx-auto px-8 py-4">
+                <h1 class="space-x-2 text-2xl -translate-y-[42px] group-hover:translate-y-0 duration-300"><span>{{ data.sl }}</span><span>{{ data.title }}</span></h1>
+                <div class="flex">
+                  <p
+                  v-for="(cats, idx) in data.tags"
+                  :key="idx"
+                  class="ml-4 bg-black/25 px-3 py-1 rounded-full -translate-y-[42px] group-hover:translate-y-0 duration-300"
+                  >
+                    {{ cats }}
+                  </p>
+                </div>
+              </div>
+              <img
+                :src="data.image_link"
+                class="w-full h-full object-cover scale-110"
+                :alt="data.title + ' image'"
+              >
+
+              <div
+                class="absolute inset-0 bg-black/0 group-hover:bg-black/50 transition-all duration-300"
+              ></div>
+            </div>
 
           </div>
         </div>
@@ -36,41 +62,55 @@ import { ref, onMounted, nextTick } from 'vue'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 
+
 import MainHeadingText from '@/components/headings/mainHeadingText.vue'
+
+import featured1 from '@/assets/images/featuredWorks/featured-1.webp';
+import featured2 from '@/assets/images/featuredWorks/featured-2.webp';
+import featured3 from '@/assets/images/featuredWorks/featured-3.webp';
+import featured4 from '@/assets/images/featuredWorks/featured-4.webp';
+import featured5 from '@/assets/images/featuredWorks/featured-5.webp';
+import featured6 from '@/assets/images/featuredWorks/featured-6.webp';
+import featured7 from '@/assets/images/featuredWorks/featured-7.webp';
+import featured8 from '@/assets/images/featuredWorks/featured-8.webp';
 
 gsap.registerPlugin(ScrollTrigger)
 
-const experiencesData = [
-  {
-    start_year: 2025,
-    end_year: "Running",
-    designation: "Frontend Engineer",
-    company: "Datascape IT Limited"
-  },
-  {
-    start_year: 2024,
-    end_year: 2025,
-    designation: "Web Design Instructor",
-    company: "Gen-Z IT Institute"
-  },
-  {
-    start_year: 2023,
-    end_year: 2024,
-    designation: "Full Stack Developer",
-    company: "Tech IT LLC"
-  }
+const featuredWorksData = [
+  { sl: '01', title: "Feature 1", tags: ["Logo", "Design"], image_link: featured1 },
+  { sl: '02', title: "Feature 2", tags: ["Logo", "Design"], image_link: featured2 },
+  { sl: '03', title: "Feature 3", tags: ["Logo", "Design"], image_link: featured3 },
+  { sl: '04', title: "Feature 4", tags: ["Logo", "Design"], image_link: featured4 },
+  { sl: '05', title: "Feature 5", tags: ["Logo", "Design"], image_link: featured5 },
+  { sl: '06', title: "Feature 6", tags: ["Logo", "Design"], image_link: featured6 },
+  { sl: '07', title: "Feature 7", tags: ["Logo", "Design"], image_link: featured7 },
+  { sl: '08', title: "Feature 8", tags: ["Logo", "Design"], image_link: featured8 },
 ]
 
-const experiences = ref<HTMLElement[]>([])
+const sectionBorder = ref<HTMLElement | null>(null);
+const featuredWork = ref<HTMLElement[]>([]);
 
 onMounted(async () => {
   await nextTick()
-  const cards = experiences.value;
+  const cards = featuredWork.value;
+
+  const sectionBorderAnimation = gsap.from(sectionBorder.value, {
+    scaleX: 0,
+    transformOrigin: "center center",
+    duration: 2,
+  })
+
+  ScrollTrigger.create({
+    trigger: sectionBorder.value,
+    scrub:2,
+    animation: sectionBorderAnimation,
+    start: "top 90%",
+    end: "top 30%",
+  })
 
   if (!cards || cards.length === 0) return
 
   cards.forEach((card, idx) => {
-
     if (idx === cards.length - 1) return
 
     gsap.to(card, {
@@ -83,7 +123,6 @@ onMounted(async () => {
         markers: false
       }
     })
-
   })
 })
 </script>
